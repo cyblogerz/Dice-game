c6aff0232811b3733079e20c771b802115c96a58
Pranav Ajay <pranavajay2002@gmail.com>
2021-09-12 00:35:56 +0530

    Uploaded all required files

---
+++ Dice-game/index.js
@@ -0,0 +1,13 @@
+document.querySelector("button").addEventListener("click", function () {
+    let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+    let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+    document.querySelector("img.img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+    document.querySelector("img.img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").textContent = "🚩Player1 wins!";
+    } else if (randomNumber1 === randomNumber2) {
+        document.querySelector("h1").textContent = "🚩Draw!🚩";
+    } else {
+        document.querySelector("h1").textContent = "Player2 wins!🚩";
+    }
+});
